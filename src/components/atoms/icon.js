f6f8bac9cae02db39d3dac6c59754cbaf94e4a4f
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-12-22 13:34:28 +0000

    Create Icon component with the relevant icons to the app

--- src/components/atoms/icon.js
+++ src/components/atoms/icon.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import {
+  fas,
+  faClock,
+  faPencilAlt,
+  faMapMarkerAlt,
+  faCommentAlt,
+  faQuestion
+} from "@fortawesome/free-solid-svg-icons";
+// faPen, faMapMarker
 
-const Icon = () => <div>ICON</div>;
+library.add(
+  fas,
+  far,
+  faClock,
+  faPencilAlt,
+  faMapMarkerAlt,
+  faCommentAlt,
+  faQuestion
+);
+
+const Icon = ({ name, isFar }) => (
+  <FontAwesomeIcon icon={[isFar ? "far" : "fas", name]} />
+);
 
 export default Icon;
