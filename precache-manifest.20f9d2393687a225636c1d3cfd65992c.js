6c35108c5d40120fef3d6a11713fa6348d50b0a7
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-12-24 15:26:58 +0000

    Updates

---
+++ precache-manifest.20f9d2393687a225636c1d3cfd65992c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485e15cf9ffcb59079fb3a4a30779df6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35845b4d48e6abc2de55",
+    "url": "/static/css/main.c9dc7bfd.chunk.css"
+  },
+  {
+    "revision": "5ca535dd0159d6f9a8db",
+    "url": "/static/js/2.0bb27001.chunk.js"
+  },
+  {
+    "revision": "fb76c9f8f8182087f9c180bc5fcbff28",
+    "url": "/static/js/2.0bb27001.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35845b4d48e6abc2de55",
+    "url": "/static/js/main.101da9c5.chunk.js"
+  },
+  {
+    "revision": "9867c0ca8452f28f9fe2",
+    "url": "/static/js/runtime-main.1e100381.js"
+  }
+]);
